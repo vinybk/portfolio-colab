ea680c050a75c63beea40e15dca6ee1d1aa12b74
Bibek Thapa <75052457+B-KEY@users.noreply.github.com>
2024-11-08 23:23:11 +0000

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,4 @@
 export const DEV_TO_CONFIG = {
   API_URL: process.env.NEXT_PUBLIC_DEV_TO_API_URL || 'https://dev.to/api',
-  USERNAME: process.env.NEXT_PUBLIC_DEV_TO_USERNAME || 'bibek-thapa'
+  USERNAME: process.env.NEXT_PUBLIC_DEV_TO_USERNAME || 'bibek-thapa01'
 }; 
